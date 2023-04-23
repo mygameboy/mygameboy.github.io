0f8915295be0f67c4d28f526192287a9cbebc755
Kushal Hada <kus@tilde.team>
2023-04-23 12:37:54 -0400

    add css from codepen
    https://codepen.io/brundolf/pen/beagbQ

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,132 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+<div class="gameboy" id="GameBoy">
+  
+  <div class="screen-area">
+    
+    <div class="power">
+      <div class="indicator">
+        <div class="led"></div>
+        <span class="arc" style="z-index:2"></span>
+        <span class="arc" style="z-index:1"></span>
+        <span class="arc" style="z-index:0"></span>
+      </div>
+      POWER
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    
+    <canvas class="display" id="mainCanvas"></canvas>
+    
+    <div class="label">
+      <div class="title">GAME BOY</div>
+      <div class="subtitle">
+        <span class="c">C</span><!--
+     --><span class="o1">O</span><!--
+     --><span class="l">L</span><!--
+     --><span class="o2">O</span><!--
+     --><span class="r">R</span>
+      </div>
+    </div>
+    
   </div>
+  
+  <div class="nintendo">Nintendo</div>
+  
+  <div class="controls">
+    <div class="dpad">
+      <div class="up"><i class="fa fa-caret-up"></i></div>
+      <div class="right"><i class="fa fa-caret-right"></i></div>
+      <div class="down"><i class="fa fa-caret-down"></i></div>
+      <div class="left"><i class="fa fa-caret-left"></i></div>
+      <div class="middle"></div>
+    </div>
+    <div class="a-b">
+      <div class="b">B</div>
+      <div class="a">A</div>
+    </div>
+  </div>
+  
+  <div class="start-select">
+    <div class="select">SELECT</div>
+    <div class="start">START</div>
+  </div>
+  
+  <div class="speaker">
+    <div class="dot placeholder"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot placeholder"></div>
+    
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+
+    <div class="dot placeholder"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot placeholder"></div>
+  </div>
+  
+</div>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
